7c62468bc9fef762fbdc8e113deddaefb9bb3f8f
shefa <144013261+shefa-sarraj@users.noreply.github.com>
2026-06-25 11:37:40 +0300

    Update App.jsx with the default route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     return (
       <div className="auth-container">
         <Routes>
+          <Route path="/" element={<Login/>}/>
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="/reset-password" element={<ResetPassword />} />
